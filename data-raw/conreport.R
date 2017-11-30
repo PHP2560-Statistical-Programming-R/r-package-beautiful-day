b324eeb2f730cb2de205ed837e0c131c60ad3fe7
Yiquan Xu <yiquan_xu@brown.edu>
2017-11-30 15:21:55 -0500

    import data

--- data-raw/conreport.R
+++ data-raw/conreport.R
@@ -1,2 +1,3 @@
+#The Air Quality Statistics Report displays air pollution values related to national standards for air quality.
 conreport2017<-read.csv("conreport2017.csv")
 devtools::use_data(conreport2017,overwrite = T)
