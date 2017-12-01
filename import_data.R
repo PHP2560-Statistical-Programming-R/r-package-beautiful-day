feeb30eccfdc16c8d9cc0461a1e42c8928d93055
Annie Yang <xiaofei_yang@brown.edu>
2017-11-30 21:20:28 -0500

    Import data before using package

---
+++ import_data.R
@@ -0,0 +1,9 @@
+# load the rda file and import the air pollution data in US
+
+# The pollution_us dataset contains four major pollutants (Nitrogen Dioxide, Sulphur Dioxide, Carbon Monoxide and Ozone) for every day from 2000-2016.
+
+load(file = "data/pollution_us.rda")
+
+# load the annual AQI data from 2000 to 2017 by Core Based Statistical Area (CBSA)
+
+load(file = "data/annual_aqi.rda")
--- import_pollution_us.R
+++
@@ -1,5 +0,0 @@
-# load the rda file and import the air pollution data in US
-
-# The dataset contains four major pollutants (Nitrogen Dioxide, Sulphur Dioxide, Carbon Monoxide and Ozone) for every day from 2000-2016.
-
-load(file = "data/pollution_us.rda")
