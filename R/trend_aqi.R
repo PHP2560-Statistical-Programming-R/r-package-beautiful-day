416b1040357026b0db19c94b24f78aa1ff669a4d
Annie Yang <xiaofei_yang@brown.edu>
2017-11-30 19:31:38 -0500

    update function

--- R/trend_aqi.R
+++ R/trend_aqi.R
@@ -1,27 +1,26 @@
 #' @title Plot AQI trend
 #'
-#' @description This function plot NO2 AQI trend since 2000
+#' @description This function plots NO2, O3, SO2 and AQI trend in US city since 2000
 #' @param data a data.frame
-#' @param city city name in US
+#' @param city US city name
 #' @seealso
-#' @ruturn
 #' @examples
 
 trend_aqi <- function (data=pollution_us, city) {
   table<-data %>%
-    select ("State", "County", "City", "Date Local",ends_with("AQI")) %>%
+    select ("City", "Date Local",ends_with("AQI")) %>%
     filter (City==city )
   
   yyyymm <- paste(format(as.POSIXlt(table$'Date Local'), format = "%y-%m"), "01", sep = "-")
   
-  monthly_mean <- tapply (table$'NO2 AQI', yyyymm, mean)
-  monthly_mean <- as.data.frame(monthly_mean[2:nrow(monthly_mean)])
+  AQI_df <- table %>% select (ends_with("AQI"))
   
+  monthly_mean<-as.data.frame(apply(AQI_df, 2, function(x) tapply(x, yyyymm, mean,na.rm=TRUE)))
   
   start_year <- as.numeric(paste0("20",substr(rownames(monthly_mean)[1], 1,2)))
   start_month <- as.numeric(substr(rownames(monthly_mean)[1], 4,5))
   
   monthly_mean <- ts(monthly_mean, start = c(start_year, start_month), frequency = 12)
   
-  plot(monthly_mean, ylab = "NO2 AQI")
+  plot(monthly_mean,main=paste(city,"Monthly AQI Trend",sep=" "))
 }
