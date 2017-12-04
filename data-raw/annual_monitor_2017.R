4e400c39ed3b31581105a2719a23b30ba6f69255
Annie Yang <xiaofei_yang@brown.edu>
2017-12-03 21:37:33 -0500

    delete dataset

--- data-raw/annual_monitor_2017.R
+++
@@ -1,3 +0,0 @@
-#This is a annual monitor file of 2017.
-annual_monitor_2017<-read.csv("annual_conc_by_monitor_2017.csv")
-devtools::use_data(annual_monitor_2017,overwrite = T)
--- data-raw/conreport.R
+++
@@ -1,3 +0,0 @@
-#The Air Quality Statistics Report displays air pollution values related to national standards for air quality.
-conreport2017<-read.csv("conreport2017.csv")
-devtools::use_data(conreport2017,overwrite = T)
