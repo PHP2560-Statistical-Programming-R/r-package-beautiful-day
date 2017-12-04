7d3c7008ee18d0495e38d1e4e67191fae96320ba
Annie Yang <xiaofei_yang@brown.edu>
2017-12-03 21:18:56 -0500

    add dataset

---
+++ data-raw/Temperature.R
@@ -0,0 +1,12 @@
+library(readr)
+
+# GlobalTemperatures.csv contains data about Global Land and Ocean-and-Land Temperatures. 
+# Starts in 1750 for average land temperature and 1850 for max and min land temperatures and global ocean and land temperatures.
+
+GlobalTemperatures <- read_csv("data-raw/GlobalTemperatures.csv")
+devtools::use_data(GlobalTemperatures, overwrite = TRUE)
+
+# GlobalLandTemperaturesByCountry.csv contains data about Global Average Land Temperature by Country.
+
+GlobalLandTemperaturesByCountry<- read_csv("data-raw/GlobalLandTemperaturesByCountry.csv")
+devtools::use_data(GlobalLandTemperaturesByCountry, overwrite = TRUE)
