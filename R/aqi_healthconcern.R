40704ca9ad96c8dfa52035450562ab156f930a01
Annie Yang <xiaofei_yang@brown.edu>
2017-12-09 01:27:14 -0500

    updated aqi_healthconcern function

--- R/aqi_healthconcern.R
+++ R/aqi_healthconcern.R
@@ -6,6 +6,8 @@
 #' @param cbsa A vector of CBSA. You can input the CBSAs you want to compare.
 #' @examples
 #' aqi_healthconcern(annual_aqi,c("Aberdeen, SD","Adrian, MI"))
+#' 
+#' @export
 
 aqi_healthconcern<-function(data=annual_aqi,cbsa){
   
@@ -27,22 +29,54 @@ aqi_healthconcern<-function(data=annual_aqi,cbsa){
     filter(CBSA %in% cbsa) # Calculate the percentage of each AQI category.
     
     Good <- ggplot(healthconcern,aes(x=Year, y=Good_Perc, color=CBSA))+
-    geom_line()
+    geom_line()+
+      labs(title = "Good",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
     
     Moderate <- ggplot(healthconcern,aes(x=Year, y=Moderate_Perc, color=CBSA))+
-      geom_line()
+      geom_line()+
+      labs(title = "Moderate",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
     
     Unhealthy_sen <- ggplot(healthconcern,aes(x=Year, y=Unhealthy_For_Sensitive_Group_Perc, color=CBSA))+
-      geom_line()
+      geom_line()+
+      labs(title = "Unhealthy for Sensitive Groups",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
     
     Unhealthy <- ggplot(healthconcern,aes(x=Year, y=Unhealthy_Perc, color=CBSA))+
-      geom_line()
+      geom_line()+
+      labs(title = "Unhealthy",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
     
     Very_Unhealthy <- ggplot(healthconcern,aes(x=Year, y=Very_Unhealthy_Perc, color=CBSA))+
-      geom_line()
+      geom_line()+
+      labs(title = "Very Unhealthy",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
     
     Hazardous <- ggplot(healthconcern,aes(x=Year, y=Hazardous_Perc, color=CBSA))+
-      geom_line()
+      geom_line()+
+      labs(title = "Hazardous",
+           x="Year",
+           y="Percentage")+
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5))
+      
     
-   ggarrange(Good, Moderate, Unhealthy_sen, Unhealthy, Very_Unhealthy, Hazardous, ncol=2,nrow=3) # Combine multiple ggplots on the same page.
+    ggpubr::ggarrange(Good, Moderate, Unhealthy_sen, Unhealthy, Very_Unhealthy, Hazardous, 
+             ncol=2,nrow=3) # Combine multiple ggplots on the same page.
 }
