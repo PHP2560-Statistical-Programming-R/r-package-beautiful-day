a67d37119ca97d12f51fd28c5cb58197d987000b
Yiquan Xu <yiquan_xu@brown.edu>
2017-12-10 20:14:35 -0500

    updata

--- data-raw/annual_aqi.R
+++ data-raw/annual_aqi.R
@@ -2,5 +2,5 @@
 
 library(readr)
 
-annual_aqi <- read_csv("data-raw/annual_aqi_by_cbsa_2000-2017.csv")
+annual_aqi <- read_csv("data-raw/annual_aqi_by_cbsa.csv")
 devtools::use_data(annual_aqi, overwrite = TRUE)
