abb473a906cde3c2928e9127f5f751cae75449b2
Annie Yang <xiaofei_yang@brown.edu>
2017-12-11 21:10:15 -0500

    update functions

--- R/aqi_healthconcern.R
+++ R/aqi_healthconcern.R
@@ -4,79 +4,49 @@
 #'     plots percentage of each category by CBSA. It can be used to compare AQI among different CBSAs.
 #' @param data A data.frame. The default dataset is annual_aqi.
 #' @param cbsa A vector of CBSA. You can input the CBSAs you want to compare.
+#' @param category A character vector of health concern category by AQI. There are six categories.
+#'     Input category from "Good", "Moderate", "UnhealthyForSensitiveGroup", "Unhealthy", "VeryUnhealthy" and "Hazardous".
+#'     Default value includes all six categories.
+#' @param year A numeric vector.
+#' @param plot A character. The value is either "line" or "bar".
+#'     Default value is "line". You can use it to decide which kind of plot you want to get.
 #' @examples
-#' aqi_healthconcern(annual_aqi,c("Aberdeen, SD","Adrian, MI"))
-#' 
+#' aqi_healthconcern(annual_aqi,c("Aberdeen, SD","Adrian, MI"),
+#' category =c("Good","Moderate","Unhealthy"),year=c(2000,2008,2015),plot="bar")
+#'
 #' @export
 
-aqi_healthconcern<-function(data=annual_aqi,cbsa){
-  
-  if (!("ggpubr" %in% installed.packages())){
-    devtools::install_github("kassambara/ggpubr")
-  }
-  
-    library(ggpubr) # This function needs ggpubr to combine multiple ggplots on the same page.
-  
+aqi_healthconcern<-function(data=annual_aqi,cbsa,
+                            category=c("Good", "Moderate", "UnhealthyForSensitiveGroup", "Unhealthy", "VeryUnhealthy","Hazardous"),
+                            year=c(2000:2015),plot="line"){
+
+
   healthconcern <- data%>%
     mutate(
-      Good_Perc = as.numeric(data$'Good Days')/as.numeric(data$'Days with AQI'),
-      Moderate_Perc = as.numeric(data$'Moderate Days')/as.numeric(data$'Days with AQI'),
-      Unhealthy_For_Sensitive_Group_Perc = as.numeric(data$'Unhealthy for Sensitive Groups Days')/as.numeric(data$'Days with AQI'),
-      Unhealthy_Perc = as.numeric(data$'Unhealthy Days')/as.numeric(data$'Days with AQI'),
-      Very_Unhealthy_Perc = as.numeric(data$'Very Unhealthy Days')/as.numeric(data$'Days with AQI'),
-      Hazardous_Perc = as.numeric(data$'Hazardous Days')/as.numeric(data$'Days with AQI')
-           )%>%
-    filter(CBSA %in% cbsa) # Calculate the percentage of each AQI category.
-    
-    Good <- ggplot(healthconcern,aes(x=Year, y=Good_Perc, color=CBSA))+
-    geom_line()+
-      labs(title = "Good",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-    Moderate <- ggplot(healthconcern,aes(x=Year, y=Moderate_Perc, color=CBSA))+
-      geom_line()+
-      labs(title = "Moderate",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-    Unhealthy_sen <- ggplot(healthconcern,aes(x=Year, y=Unhealthy_For_Sensitive_Group_Perc, color=CBSA))+
-      geom_line()+
-      labs(title = "Unhealthy for Sensitive Groups",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-    Unhealthy <- ggplot(healthconcern,aes(x=Year, y=Unhealthy_Perc, color=CBSA))+
-      geom_line()+
-      labs(title = "Unhealthy",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-    Very_Unhealthy <- ggplot(healthconcern,aes(x=Year, y=Very_Unhealthy_Perc, color=CBSA))+
-      geom_line()+
-      labs(title = "Very Unhealthy",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-    
-    Hazardous <- ggplot(healthconcern,aes(x=Year, y=Hazardous_Perc, color=CBSA))+
-      geom_line()+
-      labs(title = "Hazardous",
-           x="Year",
-           y="Percentage")+
-      theme_bw() +
-      theme(plot.title = element_text(hjust = 0.5))
-      
-    
-    ggpubr::ggarrange(Good, Moderate, Unhealthy_sen, Unhealthy, Very_Unhealthy, Hazardous, 
-             ncol=2,nrow=3) # Combine multiple ggplots on the same page.
+      Good = as.numeric(data$'Good Days')/as.numeric(data$'Days with AQI'),
+      Moderate = as.numeric(data$'Moderate Days')/as.numeric(data$'Days with AQI'),
+      UnhealthyForSensitiveGroup = as.numeric(data$'Unhealthy for Sensitive Groups Days')/as.numeric(data$'Days with AQI'),
+      Unhealthy = as.numeric(data$'Unhealthy Days')/as.numeric(data$'Days with AQI'),
+      VeryUnhealthy = as.numeric(data$'Very Unhealthy Days')/as.numeric(data$'Days with AQI'),
+      Hazardous = as.numeric(data$'Hazardous Days')/as.numeric(data$'Days with AQI')
+    )%>%
+    select(CBSA,Year,category)%>%
+    filter(CBSA %in% cbsa & Year %in% year) # Calculate the percentage of each AQI category.
+
+  if(plot=="line"){
+    healthconcern %>%
+      tidyr::gather("id", "Percentage", (ncol(healthconcern)-length(category)+1):ncol(healthconcern)) %>%
+      ggplot(., aes(Year, Percentage, color=CBSA))+
+      geom_smooth(method = "loess", se=FALSE)+
+      facet_wrap(~id,scales="free_y")+
+      theme_bw()
+  } else{
+    healthconcern %>%
+      tidyr::gather("id", "Percentage", (ncol(healthconcern)-length(category)+1):ncol(healthconcern)) %>%
+      ggplot(., aes(Year, Percentage))+
+      geom_bar(stat = "identity",aes(fill = CBSA), position = "dodge")+
+      facet_wrap(~id,scales="free_y")+
+      theme_bw()
+  }
+
 }
--- R/health_status.R
+++ R/health_status.R
@@ -3,18 +3,19 @@
 #' @description This function helps you to evaluate your health status
 #' @param data a data.frame
 #' @param cbsa cbsa in US
+#' @param year A numeric vector
 #' @param smoke whether people smoke or not, equal to -1 if you smoke, else is 1. Default is 1
 #' @param exercise whether people has certain exercise every day, equal to -1 if you don't, else is 1. Default is 1
 #' @param gene whether people has certain gene may cause disease happen,equal to -1 for bad gene or 1 for good gene. Default is 1
 #' @seealso
 #' @ruturn
 #' @examples
-#' 
+#'
 #' @export
 
 
 
-health_status<-function(data=annual_aqi_by_cbsa, cbsa, year, smoke=1, exercise=1, gene=1){
+health_status<-function(data=annual_aqi, cbsa, year, smoke=1, exercise=1, gene=1){
   env_status=0
   health_data <- data%>%filter(CBSA %in% cbsa & Year %in% year)
   names(health_data)[13] <- "MedianAQI"
@@ -23,7 +24,7 @@ health_status<-function(data=annual_aqi_by_cbsa, cbsa, year, smoke=1, exercise=1
   } else if(mean(health_data$MedianAQI)>50&mean(health_data$MedianAQI)<=100){
     env_status = env_status + 1
   } else if(mean(health_data$MedianAQI)>100&mean(health_data$MedianAQI)<=150){
-    env_status = env_status 
+    env_status = env_status
   } else if(mean(health_data$MedianAQI)>150&mean(health_data$MedianAQI)<=200){
     env_status = env_status - 1
   } else if(mean(health_data$MedianAQI)>200&mean(health_data$MedianAQI)<=300){
@@ -31,7 +32,7 @@ health_status<-function(data=annual_aqi_by_cbsa, cbsa, year, smoke=1, exercise=1
   } else{
     env_status = env_status - 3
   }
-  status = env_status + smoke + exercise + gene 
+  status = env_status + smoke + exercise + gene
   if(status>=3&status<=6){
     print('Healthy!Keep going!:)')
   } else if(status>=-2&status<=2){
