5a073ee6979c6616fb9ae79d3f3452f271d110e9
Yiquan Xu <yiquan_xu@brown.edu>
2017-12-13 15:14:53 -0500

    update

--- R/stat.R
+++ R/stat.R
@@ -10,16 +10,21 @@
 #' @export
 #' Loading packages
 #' library(dplyr)
+#' library(Rmisc)
 
 
 stat_func<-function(data=annual_aqi,cbsa){
-  names(data)[13] <- "MedianAQI"
-  
   data_stat<-data%>%
-    filter(CBSA==cbsa)%>%
-    summarise(avg=mean(MedianAQI),s_d=sd(MedianAQI),
-              Max=max(MedianAQI),Min=min(MedianAQI),
-              Range=max(MedianAQI)-min(MedianAQI))
-  
-  return(data_stat)
+    filter(CBSA==cbsa)
+  stat_table <- as.array(summary(data_stat$`Median AQI`))
+  stat_df <- as.data.frame(stat_table)
+  colnames(stat_df) <- c("Stat", "Value")
+  Confi_Interval <- CI(data_stat$`Median AQI`, ci = 0.95) #Caculate the confidence interval of Median AQI
+  lower <- data.frame('CI lower',Confi_Interval[['lower']])
+  upper <- data.frame('CI upper',Confi_Interval[['upper']])
+  names(lower) <- c("Stat", "Value")
+  names(upper) <- c("Stat", "Value")
+  stat_df <- rbind(stat_df,lower, upper)
+  return(stat_df)
 }
+
